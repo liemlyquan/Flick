eb978424b1a87c101e86a862ed58e94c7d1fb5b5
Liem Ly Quan <liem.ly@cangroup.io>
2016-03-13 06:45:05 +0700

    Add tab bar

--- Flick/MoviesViewController.swift
+++ Flick/MoviesViewController.swift
@@ -20,6 +20,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
   var haveNetworkConnection:Bool?
   var filteredData = [NSDictionary]()
   var isSearching:Bool = false
+  var endpoint = String()
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -96,7 +97,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
   
   func refreshControlAction(refreshControl: UIRefreshControl){
     let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-    let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+    let url = NSURL(string: "https://api.themoviedb.org/3/movie/\(endpoint)?api_key=\(apiKey)")
     let request = NSURLRequest(URL: url!)
     let session = NSURLSession(
       configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
@@ -148,7 +149,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
   func fetchData(){
     EZLoadingActivity.show("Loading", disableUI: true)
     let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-    let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+    let url = NSURL(string: "https://api.themoviedb.org/3/movie/\(endpoint)?api_key=\(apiKey)")
     let request = NSURLRequest(URL: url!)
     let session = NSURLSession(
       configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
--- README.md
+++ README.md
@@ -3,8 +3,10 @@
 A quick program to show now playing and top rated movie based on the database from [The Movie Database API](http://docs.themoviedb.apiary.io/)
 
 Time spent (in detailed and sequence):
-- 0.5 (mostly waiting for Pod installation)
+- 0.5 (init and some truly basic code, mostly waiting for Pod installation)
 - 2.5 (most of basic, except for detailed view and controller)
 - 0.5 (add a truly basic details view, with just an image view)
 - 3.5 (add scrollview into details, improve network checking, and fixing some issues on the way)
 - 0.5 (add basic search bar, works, not tested with different interaction yet)
+- 0.5 (add tab bar)
+
